1fdea2ff67e4603044718f74935cf9de15f799ac
prince-sojitra <sprins1353@gmail.com>
2024-05-13 10:41:53 +0530

    all ohke fone

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
-
+let router = useRouter()
 export default function Home() {
+  useEffect(() => {
+    router.push('/admin')
+  },router)
   return (
     <>
       <Head>
